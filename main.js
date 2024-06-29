2e659cfeb5f60aea15cbed2bf9433be5f21ed836
Jovan Obrencevic <115041465+Kirgishki@users.noreply.github.com>
2024-06-29 15:41:33 +0200

    Make function that get input by user Make prompt to get user choice, acceptable values are rock, paper, scissors, 1, 2, 3

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ function getComputerChoice(){
 
     return choice;
 }
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+
+    switch (humanChoice) {
+        case '1':
+            return 'rock';
+        case '2':
+            return 'paper';
+        case '3':
+            return 'scissors';
+        case 'rock':
+        case 'paper':
+        case 'scissors':
+            return humanChoice;
+
+        default:
+            alert("Wrong value!");
+            return getHumanChoice();
+    }
+}
