5cc5edc9b019b2ac66c5768adef24c0d15b0c1b3
Jovan Obrencevic <115041465+Kirgishki@users.noreply.github.com>
2024-06-29 15:45:40 +0200

    Add variables for scores

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getComputerChoice(){
     let choices = ['rock', 'paper', 'scissors'];
     let choice  = choices[Math.floor(Math.random() * choices.length)];
